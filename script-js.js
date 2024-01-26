32a07b70eabb48d9f827febb0300de1b6569885b
Weeglem <OmarWauelwgk12@gmail.com>
2024-01-26 16:39:32 -0300

    Add files via upload

--- script-js.js
+++ script-js.js
@@ -1,23 +1,24 @@
 
-//Inicia proceso
+function Inicio(){
+    //Comandos de inicio, invisiblisar el cuadro de resultado 
+    document.getElementById("Texto-Encriptado").style.display = "none";
+    document.getElementById("Boton-Copiar").style.display = "none";
+    //Limpiar la memoria de lo que escriba el usuario por seguridad
+    
+}
 
-/*
-let textoingesadeusuario = prompt("Escribe un texto a encriptar");
-let desencriptar = prompt("2");
-console.log("El usuario ingreso " + textoingesadeusuario);
-console.log("desencriptar es: " +desencriptar);
-*/
-//separar texto del usuario
+//Ejecutar Inicio
+Inicio();
 
 
 function EncriptacionMetodo(){
-        var z = textoingesadeusuario
-        .replace(/e/g,"enter")
-        .replace(/i/g,"imes")
-        .replace(/a/g,"ai")
-        .replace(/o/g,"ober")
-        .replace(/u/g,"ufat");
-        return z;
+    var z = textoingesadeusuario
+    .replace(/e/g,"enter")
+    .replace(/i/g,"imes")
+    .replace(/a/g,"ai")
+    .replace(/o/g,"ober")
+    .replace(/u/g,"ufat");
+    return z;
 }
 
 function DesencriptacionMetodo(){
@@ -31,143 +32,109 @@ function DesencriptacionMetodo(){
 }
 
 
-/* NONONO+
 
-function BotonEncriptar(){
-
-let CajaAEncriptar = document.getElementById("EncriptarContenedor");
-
-
-if(CajaAEncriptar.value == ""){
-    alert("Caja vacia");
-}else{
-    
-   textoingesadeusuario = cajaencriptadoravalor;
-   let replica = EncriptacionMetodo();
-   console.log(replica);
-   document.getElementById("Mensaje").innerHTML = replica;
-   document.getElementById("MensajeNoEncontrado").style.display  = "none";
-    document.getElementById("FotoMensaje").style.display  = "none";
-    document.getElementById("Mensaje").style.textAlign = "left";
-    document.getElementById("Mensaje").innerHTML = "uwu";
-   
-
-}
-}
-
-*/
-
-
-/*
-function prueba(){
-document.getElementById("MensajeNoEncontrado").style.display  = "none";
-document.getElementById("FotoMensaje").style.display  = "none";
-document.getElementById("Mensaje").innerHTML = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur";
-document.getElementById("Mensaje").style.textAlign = "left"; "whatever";
+function BotonCopiarTexto(){
+    textoacopiar = document.getElementById("Texto-Encriptado").value;
+    navigator.clipboard.writeText(textoacopiar);
+    alert("Copiado al Clipboard: "+textoacopiar);
 }
 
 
-prueba();
-
-*/
-
-/*  ORIGINAL
-function BotonEncriptar(){
-//Si esto es estupido, pero no funciona fuera de la funcion .-.
-let TextoParaEncriptar = document.getElementById("Texto-A-Encriptar");
-let TextoEncriptado = document.getElementById("Texto-Encriptado");
-    
-//Guardamos los valores en textoingresadoporusuario
-textoingesadeusuario = TextoParaEncriptar.value;
-let temporal1 = EncriptacionMetodo();
-
-//Limpia la caja por si habia algo anteriormente
-console.log("temporal 1: "+temporal1);  
-//Escribe le contenido procesado en EncriptacionMetodo();2
-TextoEncriptado.value = "";
-TextoEncriptado.value = temporal1;
-console.log("Texto copiado exito Encripado"); 
-
-}*/
-
-function Inicio(){
-    document.getElementById("Texto-Encriptado").style.display = "none";
+function ComprobarMAYUSCULAS(valor){  
+    console.log(valor);
+     let x = Boolean(valor.match(/[A-Z]/g));
+     console.log("MAYUSCULAS La palabra a comprobar es: "+valor+" y devolvio "+x);
+     return x;
 }
 
-Inicio();
+function ComprobarCaracteresEspeciales(valor){  
+    console.log(valor);
+    let tabla = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/g;
+    let x = Boolean(valor.match(tabla));
+    console.log("CARACTERES La palabra a comprobar es: "+valor+" y devolvio "+x);
+    return x;
+}
 
-function BotonEncriptar(){
-    //Si esto es estupido, pero no funciona fuera de la funcion .-.
-    let TextoParaEncriptar = document.getElementById("Texto-A-Encriptar");
-    let MostrarTextoFinal = document.getElementsByClassName("grupo2");
-        
-    //Guardamos los valores en textoingresadoporusuario
-    textoingesadeusuario = TextoParaEncriptar.value;
-    let temporal1 = EncriptacionMetodo();
-    
-    //Limpia la caja por si habia algo anteriormente
-    console.log("temporal 1: "+temporal1);  
-    //Escribe le contenido procesado en EncriptacionMetodo();2
-    document.getElementById("Texto-Encriptado").value = temporal1;
-
+function DesaparecerMensajeError(){
     document.getElementById("MensajeNoEncontrado").style.display  = "none";
     document.getElementById("FotoMensaje").style.display  = "none";
     document.getElementById("Mensaje").style.display  = "none";
+
     document.getElementById("Texto-Encriptado").style.display = "block";
-
-    console.log("Texto copiado exito Encripado"); 
-    
-    }
+    document.getElementById("Boton-Copiar").style.display = "block";
+}
 
 
-/*  alternarivo modifica P
+
+
 
 function BotonEncriptar(){
+    console.log("-----------------------------------------------");
     //Si esto es estupido, pero no funciona fuera de la funcion .-.
     let TextoParaEncriptar = document.getElementById("Texto-A-Encriptar");
-    let MostrarTextoFinal = document.getElementsByClassName("grupo2");
-        
-    //Guardamos los valores en textoingresadoporusuario
     textoingesadeusuario = TextoParaEncriptar.value;
-    let temporal1 = EncriptacionMetodo();
-    
-    //Limpia la caja por si habia algo anteriormente
-    console.log("temporal 1: "+temporal1);  
-    //Escribe le contenido procesado en EncriptacionMetodo();2
-    document.getElementById("Mensaje").innerHTML = temporal1;
+        
+    //Comprobaciones
 
-    document.getElementById("MensajeNoEncontrado").style.display  = "none";
-    document.getElementById("FotoMensaje").style.display  = "none";
+    if (textoingesadeusuario == ""){
+        alert("Ingresa un mensaje primero");
+    }else{
+        if (ComprobarMAYUSCULAS(textoingesadeusuario)){
+        alert("Quitame las mayusculas boludo");
+        }else{
+        if(ComprobarCaracteresEspeciales(textoingesadeusuario)){
+        alert("Quitame los caracteres boludo"); 
+        } else{
+        //Guardamos los valores en textoingresadoporusuario
+        let temporal = EncriptacionMetodo();
+        
+        //Limpia la caja por si habia algo anteriormente
+        console.log("temporal: "+temporal);  
+        //Escribe le contenido procesado en EncriptacionMetodo();2
+        document.getElementById("Texto-Encriptado").value = temporal;
 
-    console.log("Texto copiado exito Encripado"); 
-    
+        //Borrar falta mensaje y hacer aparecer resultado
+        DesaparecerMensajeError();
+        
+
+        console.log("Encriptacion terminada"); 
     }
+    }
+    }
+}
 
-
-    */
-    
-
-
-
-
-
-
-/*  ORIGINAL
 
 function BotonDesencriptar(){
+    console.log("-----------------------------------------------");
     //Si esto es estupido, pero no funciona fuera de la funcion .-.
     let TextoParaEncriptar = document.getElementById("Texto-A-Encriptar");
-    let TextoEncriptado = document.getElementById("Texto-Encriptado");
+    textoingesadeusuario = TextoParaEncriptar.value;
         
-    //Guardamos los valores en textoingresadoporusuario
-    textoingesadeusuario = TextoEncriptado.value;
-    let temporal2 = DesencriptacionMetodo();
-    TextoParaEncriptar.value = "";
-    //Limpia la caja por si habia algo anteriormente
-    console.log("temporal 2: "+temporal2);  
-    //Escribe le contenido procesado
-    TextoParaEncriptar.value = temporal2;
-    console.log("Texto copiado exito DESncripado");   
-    }
+    //Comprobaciones
 
-    */
+    if (textoingesadeusuario == ""){
+        alert("Ingresa un mensaje primero");
+    }else{
+        if (ComprobarMAYUSCULAS(textoingesadeusuario)){
+        alert("Quitame las mayusculas boludo");
+        }else{
+        if(ComprobarCaracteresEspeciales(textoingesadeusuario)){
+        alert("Quitame los caracteres boludo"); 
+        } else{
+        //Guardamos los valores en textoingresadoporusuario
+        let temporal = DesencriptacionMetodo();
+        
+        //Limpia la caja por si habia algo anteriormente
+        console.log("temporal: "+temporal);  
+        //Escribe le contenido procesado en EncriptacionMetodo();2
+        document.getElementById("Texto-Encriptado").value = temporal;
+
+        //Borrar falta mensaje y hacer aparecer resultado
+        DesaparecerMensajeError();
+        
+
+        console.log("Desencriptacion terminada"); 
+    }
+    }
+    }
+}
